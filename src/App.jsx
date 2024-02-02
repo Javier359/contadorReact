5fde9ea20cb2cba1ed83b980012328de0734023d
Javier Jimenez <javier.sniguel@gmail.com>
2024-02-01 23:16:59 -0300

    Se agrego el componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TituloSecundario from './components/TituloSecundario'
 
 function App() {
+ // aqui agregaos toda la logica que necesita el componente
+
   return (
-   <h1 className='display-3'>Titulo de prueba</h1>
+    //aqui puedo agregar logica muy pequena
+    <>
+    {/*aqui empiezo a maquetar en el fragment o en una etiqueta contenedora */}
+   <section className='container my-4'>
+    <h1 className='display-3 text-center'>Contador con React</h1>
+    <TituloSecundario></TituloSecundario>
+   </section>
+   </>
   )
 }
 
---
+++ src/components/TituloSecundario.jsx
@@ -0,0 +1,23 @@
+const TituloSecundario = () => {
+  return (
+    <article>
+      <h2>Practica con React</h2>
+      <p>
+        React es una biblioteca de JavaScript utilizada para construir
+        interfaces de usuario interactivas y eficientes. Desarrollada y
+        mantenida por Facebook, React se centra en la creación de componentes
+        reutilizables que representan diferentes partes de la interfaz de
+        usuario. Su principal característica es el modelo de programación
+        declarativo, que permite a los desarrolladores describir cómo debería
+        verse la interfaz en un estado particular y React se encarga de
+        actualizar automáticamente la vista cuando cambian los datos. Además,
+        React facilita la construcción de aplicaciones escalables al dividir la
+        interfaz de usuario en componentes independientes y gestionar
+        eficientemente la manipulación del DOM a través de su enfoque de
+        reconciliación virtual.
+      </p>
+    </article>
+  );
+};
+
+export default TituloSecundario;
