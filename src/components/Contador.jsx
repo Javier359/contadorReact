555f3cbbeecf1e2618462ed94b2095f248f95c2d
Javier Jimenez <javier.sniguel@gmail.com>
2024-02-02 21:47:31 -0300

    agregamos un estado al componente contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react"; //ESTO ES PARA INCORPORRAR LOS ESTADOS AL COMPONENTE
 const Contador = () => {
-    let numero= 10;
-    return (
-        <article className="text-center">
-            <h2>Contador</h2>
-            <h3>{numero}</h3>
-            <button className="btn btn-primary" onClick={()=>{numero++; console.log(numero)}}>+1</button>
-        </article>
-    );
+  //creo mi estado
+  const [numero, setNumero] = useState(10); //nombre sel state, funcion del state
+  //   let numero = 10;
+//   setNumero(1); los estados se actualizan con la funcion set
+  return (
+    <article className="text-center">
+      <h2>Contador</h2>
+      <h3>{numero}</h3>
+      <button className="btn btn-primary" onClick={() => setNumero(numero + 1)}>
+        +1
+      </button>
+    </article>
+  );
 };
 
 export default Contador;
